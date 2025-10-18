8fe4693309847a40a2dbccc551f3dbb12340b8b6
Anastasiia <koicheva1996@gmail.com>
2025-10-18 18:18:05 +0300

    add: #2 path aliases, favicon and metadata, dark/light theme support

--- next-blog/eslint.config.mjs
+++ next-blog/eslint.config.mjs
@@ -1,25 +1,38 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		ignores: [
+			"node_modules/**",
+			".next/**",
+			"out/**",
+			"build/**",
+			"next-env.d.ts",
+		],
+	},
+	{
+		plugins: {
+			import: importPlugin,
+		},
+		settings: {
+			"import/resolver": {
+				typescript: {
+					project: "./tsconfig.json",
+				},
+			},
+		},
+	},
 ];
 
 export default eslintConfig;
